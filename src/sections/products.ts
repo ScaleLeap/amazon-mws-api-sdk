0c2994944a14a074f92fbc6cee1587f4bb882364
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-05-26 15:04:22 +0800

    fix: fix parameters get matching product for id

--- src/sections/products.ts
+++ src/sections/products.ts
@@ -248,10 +248,6 @@ export class Products {
     })
   }
 
-  /**
-   *
-   * @todo Think about converting GetMatchingProductResposne to array to match getMatchingProductForId
-   */
   async getMatchingProduct(
     parameters: GetMatchingProductParameters,
   ): Promise<[MatchingProduct, RequestMeta]> {
@@ -280,7 +276,11 @@ export class Products {
       resource: Resource.Products,
       version: PRODUCTS_API_VERSION,
       action: 'GetMatchingProductForId',
-      parameters,
+      parameters: {
+        'IdList.Id': parameters.IdList,
+        IdType: parameters.IdType,
+        MarketplaceId: parameters.MarketplaceId,
+      },
     })
 
     return GetMatchingProductForIdResponseCodec.decode(response).caseOf({
