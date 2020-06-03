384209755fc7105cfb254a3dd99f21edef93e760
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-06-03 20:39:53 +0800

    test: remove all except ASIN test

--- test/integration/products.test.ts
+++ test/integration/products.test.ts
@@ -18,35 +18,6 @@ const httpClient = new HttpClient({
 describe(`products`, () => {
   const products = new Products(httpClient)
   const ASINList = ['B00D6CMT12', 'B07L1G4YKT', 'B00J8NCVX4']
-  const SKUList = ['SPATULA-MWS-TEST', 'B07L1G4YKT', 'B00J8NCVX4']
-  // const InvalidASINsWithOneValidASIN = ['X', 'The next ID is valid', 'B00D6CMT12']
-
-  // itci('should properly decode request with one valid ASIN', async () => {
-  //   expect.assertions(1)
-
-  //   const [getMatchingProductResponse] = await products.getMatchingProduct({
-  //     ASINList,
-  //     MarketplaceId: amazonMarketplaces.CA.id,
-  //   })
-
-  //   /**
-  //    * @todo: Review returned data and adjust API accordingly
-  //    * I don't actually know what to expect here yet
-  //    */
-  //   expect(Array.isArray(getMatchingProductResponse)).toBe(true)
-  // })
-
-  itci('should get matching products of more than one sku', async () => {
-    expect.assertions(1)
-
-    const [getMatchingProductResponse] = await products.getMatchingProductForId({
-      IdList: SKUList,
-      MarketplaceId: amazonMarketplaces.CA.id,
-      IdType: 'SKU',
-    })
-
-    expect(Array.isArray(getMatchingProductResponse)).toBe(true)
-  })
 
   itci('should get matching products of more than one asin', async () => {
     expect.assertions(1)
