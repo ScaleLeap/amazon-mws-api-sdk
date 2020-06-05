bf57f3f9340afb2592b41d48f4efe7b0cc8e4c0b
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-06-05 15:48:50 +0800

    feat: add require only one type

---
+++ src/sections/types.ts
@@ -0,0 +1,8 @@
+/**
+ * For use with parameters that require one of two values
+ */
+
+type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> &
+  {
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Record<Exclude<Keys, K>, undefined>>
+  }[Keys]
