c01036899592e8477a5e1da928745055e06d19c6
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-06-05 20:30:46 +0800

    test: unit tests get report request list

--- test/unit/reports.test.ts
+++ test/unit/reports.test.ts
@@ -20,35 +20,46 @@ const headers = {
 
 const parsingError = 'Expected an object, but received a string with value ""'
 
-const mockRequestReport = new MWS(
-  new HttpClient(httpConfig, () =>
-    Promise.resolve({
-      data: getFixture('reports_request_report'),
-      headers,
-    }),
-  ),
-)
+const createMockHttpClient = (fixture: string) =>
+  new MWS(
+    new HttpClient(httpConfig, () =>
+      Promise.resolve({
+        data: getFixture(fixture),
+        headers,
+      }),
+    ),
+  )
+
+const mockMwsServiceStatus = createMockHttpClient('get_service_status')
 
 const mockMwsFail = new MWS(
   new HttpClient(httpConfig, () => Promise.resolve({ data: '', headers: {} })),
 )
 
-const mockMwsServiceStatus = new MWS(
-  new HttpClient(httpConfig, () =>
-    Promise.resolve({
-      data: getFixture('get_service_status'),
-      headers: {
-        'x-mws-request-id': '0',
-        'x-mws-timestamp': '2020-05-06T08:22:23.582Z',
-        'x-mws-quota-max': '1000',
-        'x-mws-quota-remaining': '999',
-        'x-mws-quota-resetson': '2020-05-06T10:22:23.582Z',
-      },
-    }),
-  ),
-)
+const mockRequestReport = createMockHttpClient('reports_request_report')
+const mockGetReportRequestList = createMockHttpClient('reports_get_report_request_list')
 
 describe('reports', () => {
+  describe('getReportRequestList', () => {
+    const parameters = {}
+
+    it('returns a parsed model when the response is valid', async () => {
+      expect.assertions(1)
+
+      expect(
+        await mockGetReportRequestList.reports.getReportRequestList(parameters),
+      ).toMatchSnapshot()
+    })
+
+    it('throws a parsing error when the response is not valid', async () => {
+      expect.assertions(1)
+
+      await expect(() =>
+        mockMwsFail.reports.getReportRequestList(parameters),
+      ).rejects.toStrictEqual(new ParsingError(parsingError))
+    })
+  })
+
   describe('requestReport', () => {
     const parameters = {
       ReportType: '',
