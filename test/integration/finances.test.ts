e4b5fec55a9d2f07676639fedb7526c631588a6d
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-06-19 21:44:04 +0800

    test: add basic integration tests

--- test/integration/finances.test.ts
+++ test/integration/finances.test.ts
@@ -6,10 +6,29 @@ const httpClient = new Config().createHttpClient()
 
 /* eslint-disable jest/no-standalone-expect */
 describe(`${Finances.name}`, () => {
-  itci('should be able to query service status', async () => {
+  const finances = new Finances(httpClient)
+  itci('list financial event groups should post succesfully with a basic request', async () => {
     expect.assertions(1)
 
-    const finances = new Finances(httpClient)
+    const [response] = await finances.listFinancialEventGroups({
+      FinancialEventGroupStartedAfter: new Date('2018/01/01'),
+    })
+
+    expect(response).toBeDefined()
+  })
+
+  itci('list financial events should post succesfully with a basic request', async () => {
+    expect.assertions(1)
+
+    const [response] = await finances.listFinancialEvents({
+      PostedAfter: new Date('2018/01/01'),
+    })
+
+    expect(response).toBeDefined()
+  })
+
+  itci('should be able to query service status', async () => {
+    expect.assertions(1)
 
     const [response] = await finances.getServiceStatus()
 
