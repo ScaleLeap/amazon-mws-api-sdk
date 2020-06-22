a1e5c8b1f33a7ed7a159a0dcdb1a92b4ecf3720e
Roman Filippov <rf@romanfilippov.com>
2020-06-22 15:18:48 +0700

    test: create subscription

--- test/integration/subscriptions.test.ts
+++ test/integration/subscriptions.test.ts
@@ -1,3 +1,5 @@
+import { amazonMarketplaces } from '@scaleleap/amazon-marketplaces'
+
 import { Subscriptions } from '../../src'
 import { Config } from './config'
 import { itci } from './it'
@@ -14,6 +16,45 @@ describe('subscriptions', () => {
     const [response] = await subscriptions.getServiceStatus()
 
     expect(response.Status).toMatch(/GREEN|YELLOW|RED/)
+  })
+
+  itci('should be able to create a subscription', async () => {
+    expect.assertions(2)
+
+    const subscriptions = new Subscriptions(httpClient)
+
+    const [registerDestinationResponse] = await subscriptions.registerDestination({
+      MarketplaceId: amazonMarketplaces.CA.id,
+      Destination: {
+        AttributeList: [
+          {
+            Key: 'sqsQueueUrl',
+            Value: 'https://sqs.us-east-1.amazonaws.com/304786922662/mws-sub-testw',
+          },
+        ],
+        DeliveryChannel: 'SQS',
+      },
+    })
+
+    const [createSubscriptionResponse] = await subscriptions.createSubscription({
+      MarketplaceId: amazonMarketplaces.CA.id,
+      Subscription: {
+        IsEnabled: true,
+        Destination: {
+          AttributeList: [
+            {
+              Key: 'sqsQueueUrl',
+              Value: 'https://sqs.us-east-1.amazonaws.com/304786922662/mws-sub-testw',
+            },
+          ],
+          DeliveryChannel: 'SQS',
+        },
+        NotificationType: 'AnyOfferChanged',
+      },
+    })
+
+    expect(registerDestinationResponse).toBe('')
+    expect(createSubscriptionResponse).toBe('')
   })
 })
 /* eslint-enable jest/no-standalone-expect */
