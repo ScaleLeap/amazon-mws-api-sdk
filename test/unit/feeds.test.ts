9cfd96b17ee2d5410422d1bbbb18847c30029fee
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-06-23 16:56:47 +0800

    test: made tests for getFeedSubmissionList

--- test/unit/feeds.test.ts
+++ test/unit/feeds.test.ts
@@ -1,9 +1,22 @@
-// import { ParsingError } from '../../src'
-// import { mockMwsFail, mockMwsServiceStatus, parsingError } from '../utils'
+import { ParsingError } from '../../src'
+import { createMockHttpClient, mockMwsFail, parsingError } from '../utils'
 
 describe('feeds', () => {
-  it('is true', () => {
-    expect.hasAssertions()
-    expect(true).toBe(true)
+  describe('getFeedSubmissionList', () => {
+    it('returns a next token and feed submission info if succesful', async () => {
+      expect.assertions(1)
+
+      const mockGetFeedSubmissionList = createMockHttpClient('feeds_get_feed_submission_list')
+
+      expect(await mockGetFeedSubmissionList.feeds.getFeedSubmissionList()).toMatchSnapshot()
+    })
+
+    it('throws a parsing error when the response is not valid', async () => {
+      expect.assertions(1)
+
+      await expect(() => mockMwsFail.feeds.getFeedSubmissionList()()).rejects.toStrictEqual(
+        new ParsingError(parsingError),
+      )
+    })
   })
 })
