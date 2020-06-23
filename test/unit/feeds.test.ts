bf37943a1082d865a5be5bd3b4dea284dcc753ad
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-06-23 18:18:46 +0800

    test: made tests for get feed submission list by nt

--- test/unit/feeds.test.ts
+++ test/unit/feeds.test.ts
@@ -1,4 +1,5 @@
 import { ParsingError } from '../../src'
+import { NextToken } from '../../src/parsing'
 import { createMockHttpClient, mockMwsFail, parsingError } from '../utils'
 
 describe('feeds', () => {
@@ -17,6 +18,28 @@ describe('feeds', () => {
       await expect(() => mockMwsFail.feeds.getFeedSubmissionList()).rejects.toStrictEqual(
         new ParsingError(parsingError),
       )
+    })
+  })
+
+  describe('getFeedSubmissionListByNextToken', () => {
+    const nextToken = new NextToken('GetFeedSubmissionList', '123')
+
+    it('returns a next token and feed submission info if succesful', async () => {
+      expect.assertions(1)
+
+      const mockGetFeedSubmissionListNT = createMockHttpClient('feeds_get_feed_submission_list_nt')
+
+      expect(
+        await mockGetFeedSubmissionListNT.feeds.getFeedSubmissionListByNextToken(nextToken),
+      ).toMatchSnapshot()
+    })
+
+    it('throws a parsing error when the response is not valid', async () => {
+      expect.assertions(1)
+
+      await expect(() =>
+        mockMwsFail.feeds.getFeedSubmissionListByNextToken(nextToken),
+      ).rejects.toStrictEqual(new ParsingError(parsingError))
     })
   })
 })
