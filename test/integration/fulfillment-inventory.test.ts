cff6ffc5b9662ed5c20df7e41c862992beb298c6
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-06-23 20:02:07 +0800

    test: add integration tests for fulfillment inventory

---
+++ test/integration/fulfillment-inventory.test.ts
@@ -0,0 +1,35 @@
+import { FulfillmentInventory, ListInventorySupplyRequestParameters } from '../../src'
+import { Config } from './config'
+import { itci } from './it'
+
+const httpClient = new Config().createHttpClient()
+
+/* eslint-disable jest/no-standalone-expect */
+describe(`${FulfillmentInventory.name}`, () => {
+  itci('should be able to get list of inventory supply', async () => {
+    expect.assertions(1)
+
+    const date = new Date(Date.now() - 150 * 24 * 60 * 60 * 1000) // Date from 150 days ago
+
+    const parameters: ListInventorySupplyRequestParameters = {
+      QueryStartDateTime: date,
+    }
+
+    const fulfillmentInventory = new FulfillmentInventory(httpClient)
+
+    const [marketplaceParticipations] = await fulfillmentInventory.listInventorySupply(parameters)
+
+    expect(marketplaceParticipations).toBeDefined()
+  })
+
+  itci('should be able to query service status', async () => {
+    expect.assertions(1)
+
+    const fulfillmentInventory = new FulfillmentInventory(httpClient)
+
+    const [response] = await fulfillmentInventory.getServiceStatus()
+
+    expect(response.Status).toMatch(/GREEN|YELLOW|RED/)
+  })
+})
+/* eslint-enable jest/no-standalone-expect */
