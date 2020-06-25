30d2f4763246163297153eb2f409dbabd3085ade
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-06-25 21:15:28 +0800

    refactor: isolate submitFeed integration test

---
+++ test/integration/submit-feed.test.ts
@@ -0,0 +1,35 @@
+import { jestPollyConfigService } from '@scaleleap/jest-polly'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+
+import { Feeds, SubmitFeedParameters } from '../../src'
+import { Config } from './config'
+import { itci } from './it'
+
+jestPollyConfigService.config = {
+  matchRequestsBy: {
+    url: false,
+  },
+}
+
+const httpClient = new Config().createHttpClient()
+
+/* eslint-disable jest/no-standalone-expect */
+describe(`submit-feed`, () => {
+  const feeds = new Feeds(httpClient)
+  itci('should be able to submit sample feed', async () => {
+    expect.assertions(1)
+
+    const parameters: SubmitFeedParameters = {
+      FeedContent: readFileSync(join(__dirname, `/submit_feed_sample_feed_content.xml`), {
+        encoding: 'utf8',
+      }),
+      FeedType: '_POST_PRODUCT_DATA_',
+    }
+
+    const [response] = await feeds.submitFeed(parameters)
+
+    expect(response).toBeDefined()
+  })
+})
+/* eslint-enable jest/no-standalone-expect */
