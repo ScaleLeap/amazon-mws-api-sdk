692af1bfea1b6a50e1352d8fa10727824cb704d7
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-07-03 11:42:50 +0800

    fix: add temp fix to uniform TZ

--- src/parsing.ts
+++ src/parsing.ts
@@ -59,7 +59,15 @@ export const mwsBoolean = Codec.custom<boolean>({
 })
 
 export const mwsDate = Codec.custom<Date>({
-  decode: (x) => string.decode(x).chain((aString) => date.decode(decodeURIComponent(aString))),
+  decode: (x) =>
+    string.decode(x).chain((aString) => {
+      let toDecode = aString
+      // Temp fix. Add UTC if no letter is found in "param" passed
+      if (!/[A-Za-z]/g.test(aString)) {
+        toDecode += ' UTC+00:00'
+      }
+      return date.decode(decodeURIComponent(toDecode))
+    }),
   encode: date.encode,
   schema: date.schema,
 })
