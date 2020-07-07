51bc66912b09abbdecdc4b5bc2d648f724cf2807
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-07-07 09:27:18 +0800

    test: add unit test for getFbaOutboundShipmentDetail

--- src/sections/shipment-invoicing.ts
+++ src/sections/shipment-invoicing.ts
@@ -4,6 +4,8 @@ import { getServiceStatusByResource } from './shared'
 /**
  * The first API version is what's written in the docs directory name
  * but the second one is what's used in the request examples
+ * Keeping both for now as reference, and encountered an issue in integrations
+ * early on (something about incorrect address) that resolved itself for some reason
  */
 const SHIPMENT_INVOICING_API_VERSION = '2018-11-01'
 // const SHIPMENT_INVOICING_API_VERSION = '2018-09-01'
--- test/unit/shipment-invoicing.test.ts
+++ test/unit/shipment-invoicing.test.ts
@@ -1,7 +1,36 @@
 import { ParsingError } from '../../src'
-import { mockMwsFail, mockMwsServiceStatus, parsingError } from '../utils'
+import { createMockHttpClient, mockMwsFail, mockMwsServiceStatus, parsingError } from '../utils'
 
 describe('shipmentInvoicing', () => {
+  describe('getFbaOutboundShipmentDetail', () => {
+    const parameters = {
+      MarketplaceId: '',
+      AmazonShipmenId: '',
+    }
+
+    it('returns the shipment detail if succesful', async () => {
+      expect.assertions(1)
+
+      const mockGetFbaOutboundShipmentDetail = createMockHttpClient(
+        'shipment_invoicing_get_fba_outbound_shipment_detail',
+      )
+
+      expect(
+        await mockGetFbaOutboundShipmentDetail.shipmentInvoicing.getFbaOutboundShipmentDetail(
+          parameters,
+        ),
+      ).toMatchSnapshot()
+    })
+
+    it('throws a parsing error when the status response is not valid', async () => {
+      expect.assertions(1)
+
+      await expect(() =>
+        mockMwsFail.shipmentInvoicing.getFbaOutboundShipmentDetail(parameters),
+      ).rejects.toStrictEqual(new ParsingError(parsingError))
+    })
+  })
+
   describe('getServiceStatus', () => {
     it('returns a parsed model when the status response is valid', async () => {
       expect.assertions(1)
