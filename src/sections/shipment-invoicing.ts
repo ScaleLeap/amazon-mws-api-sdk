a455fbfa3a759a69c03b8b8671cbbfe303c16aa1
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-07-07 12:10:29 +0800

    fix: this is the correct API ver

--- src/sections/shipment-invoicing.ts
+++ src/sections/shipment-invoicing.ts
@@ -5,14 +5,7 @@ import { HttpClient, RequestMeta, Resource } from '../http'
 import { ensureArray, ensureString } from '../parsing'
 import { getServiceStatusByResource } from './shared'
 
-/**
- * The first API version is what's written in the docs directory name
- * but the second one is what's used in the request examples
- * Keeping both for now as reference, and encountered an issue in integrations
- * early on (something about incorrect address) that resolved itself for some reason
- */
-const SHIPMENT_INVOICING_API_VERSION = '2018-11-01'
-// const SHIPMENT_INVOICING_API_VERSION = '2018-09-01'
+const SHIPMENT_INVOICING_API_VERSION = '2018-09-01'
 
 export interface GetFbaOutboundShipmentDetailParameters {
   MarketplaceId: string
