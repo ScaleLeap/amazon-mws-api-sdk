27756500fe6e90a47415666bc46a46c8d2f8ccd2
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-07-07 08:50:15 +0800

    fix: check both api vers

--- src/sections/shipment-invoicing.ts
+++ src/sections/shipment-invoicing.ts
@@ -5,8 +5,8 @@ import { getServiceStatusByResource } from './shared'
  * The first API version is what's written in the docs directory name
  * but the second one is what's used in the request examples
  */
-// const SHIPMENT_INVOICING_API_VERSION = '2018-11-01'
-const SHIPMENT_INVOICING_API_VERSION = '2018-09-01'
+const SHIPMENT_INVOICING_API_VERSION = '2018-11-01'
+// const SHIPMENT_INVOICING_API_VERSION = '2018-09-01'
 export class ShipmentInvoicing {
   constructor(private httpClient: HttpClient) {}
 
