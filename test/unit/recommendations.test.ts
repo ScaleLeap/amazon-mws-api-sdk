40dfc2641138f6b2814011280467be3aee614aa2
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-07-08 13:51:28 +0800

    test: unit tests for getLastUpdatedTimeForRecommendations

--- test/unit/recommendations.test.ts
+++ test/unit/recommendations.test.ts
@@ -1,7 +1,35 @@
 import { ParsingError } from '../../src'
-import { mockMwsFail, mockMwsServiceStatus, parsingError } from '../utils'
+import { createMockHttpClient, mockMwsFail, mockMwsServiceStatus, parsingError } from '../utils'
 
 describe('recommendations', () => {
+  describe('getLastUpdatedTimeForRecommendations', () => {
+    const parameters = {
+      MarketplaceId: '',
+    }
+
+    it('returns last updated time for recommendations if succesful', async () => {
+      expect.assertions(1)
+
+      const mockGetLastUpdatedTimeForRecommendations = createMockHttpClient(
+        'recommendations_get_last_updated_time_for_recommendations',
+      )
+
+      expect(
+        await mockGetLastUpdatedTimeForRecommendations.recommendations.getLastUpdatedTimeForRecommendations(
+          parameters,
+        ),
+      ).toMatchSnapshot()
+    })
+
+    it('throws a parsing error when the status response is not valid', async () => {
+      expect.assertions(1)
+
+      await expect(() =>
+        mockMwsFail.recommendations.getLastUpdatedTimeForRecommendations(parameters),
+      ).rejects.toStrictEqual(new ParsingError(parsingError))
+    })
+  })
+
   describe('getServiceStatus', () => {
     it('returns a parsed model when the status response is valid', async () => {
       expect.assertions(1)
