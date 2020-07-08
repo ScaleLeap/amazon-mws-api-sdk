1236a8947d609ed5c3a65c1d331e6990971146b0
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-07-08 14:13:29 +0800

    test: add integration test, check for actual structure

--- test/integration/recommendations.test.ts
+++ test/integration/recommendations.test.ts
@@ -1,4 +1,4 @@
-import { Recommendations } from '../../src'
+import { amazonMarketplaces, Recommendations } from '../../src'
 import { Config } from './config'
 import { itci } from './it'
 
@@ -6,6 +6,17 @@ const httpClient = new Config().createHttpClient()
 
 /* eslint-disable jest/no-standalone-expect */
 describe(`${Recommendations.name}`, () => {
+  itci('should be able to query last updated time for recommendations', async () => {
+    expect.assertions(1)
+
+    const recommendations = new Recommendations(httpClient)
+
+    const [response] = await recommendations.getLastUpdatedTimeForRecommendations({
+      MarketplaceId: amazonMarketplaces.US.id,
+    })
+
+    expect(response).toBeDefined()
+  })
   itci('should be able to query service status', async () => {
     expect.assertions(1)
 
