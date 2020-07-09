15e57961065156fd3f7d6b79802813df3a192c50
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-07-09 17:00:27 +0800

    test: unit tet getInboundGuidanceForAsin

--- test/unit/fulfillment-inbound-shipment.test.ts
+++ test/unit/fulfillment-inbound-shipment.test.ts
@@ -2,6 +2,35 @@ import { ParsingError } from '../../src'
 import { createMockHttpClient, mockMwsFail, mockMwsServiceStatus, parsingError } from '../utils'
 
 describe('fulfillmentInboundShipment', () => {
+  describe('getInboundGuidanceForAsin', () => {
+    const parameters = {
+      ASINList: [],
+      MarketplaceId: '',
+    }
+
+    it('returns sku inbound guidance list if succesful', async () => {
+      expect.assertions(1)
+
+      const mockGetInboundGuidanceForSku = createMockHttpClient(
+        'fulfillment_inbound_shipment_get_inbound_guidance_for_asin',
+      )
+
+      expect(
+        await mockGetInboundGuidanceForSku.fulfillmentInboundShipment.getInboundGuidanceForAsin(
+          parameters,
+        ),
+      ).toMatchSnapshot()
+    })
+
+    it('throws a parsing error when the status response isnt valid', async () => {
+      expect.assertions(1)
+
+      await expect(() =>
+        mockMwsFail.fulfillmentInboundShipment.getInboundGuidanceForAsin(parameters),
+      ).rejects.toStrictEqual(new ParsingError(parsingError))
+    })
+  })
+
   describe('getInboundGuidanceForSku', () => {
     const parameters = {
       SellerSKUList: [],
