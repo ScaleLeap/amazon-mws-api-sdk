55f7dbecba4d769dc62c74598271e16b8f732396
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-07-09 15:52:18 +0800

    test: unit test for getInboundGuidanceForSku

--- test/unit/fulfillment-inbound-shipment.test.ts
+++ test/unit/fulfillment-inbound-shipment.test.ts
@@ -1,7 +1,36 @@
 import { ParsingError } from '../../src'
-import { mockMwsFail, mockMwsServiceStatus, parsingError } from '../utils'
+import { createMockHttpClient, mockMwsFail, mockMwsServiceStatus, parsingError } from '../utils'
 
 describe('fulfillmentInboundShipment', () => {
+  describe('getInboundGuidanceForSku', () => {
+    const parameters = {
+      SellerSKUList: [],
+      MarketplaceId: '',
+    }
+
+    it('returns sku inbound guidance list if succesful', async () => {
+      expect.assertions(1)
+
+      const mockGetInboundGuidanceForSku = createMockHttpClient(
+        'fulfillment_inbound_shipment_get_inbound_guidance_for_sku',
+      )
+
+      expect(
+        await mockGetInboundGuidanceForSku.fulfillmentInboundShipment.getInboundGuidanceForSku(
+          parameters,
+        ),
+      ).toMatchSnapshot()
+    })
+
+    it('throws a parsing error when the status response is not valid', async () => {
+      expect.assertions(1)
+
+      await expect(() =>
+        mockMwsFail.fulfillmentInboundShipment.getInboundGuidanceForSku(parameters),
+      ).rejects.toStrictEqual(new ParsingError(parsingError))
+    })
+  })
+
   describe('getServiceStatus', () => {
     it('returns a parsed model when the status response is valid', async () => {
       expect.assertions(1)
