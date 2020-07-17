c932b0c25e52a9ae75dc9f0cc0620f0ece4e030c
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-07-17 18:46:59 +0800

    test: unit test createFulfillmentOrder

--- test/unit/fulfillment-outbound-shipment.test.ts
+++ test/unit/fulfillment-outbound-shipment.test.ts
@@ -1,21 +1,61 @@
 import { ParsingError } from '../../src'
 import { createMockHttpClient, mockMwsFail, mockMwsServiceStatus, parsingError } from '../utils'
 
+const mockAddress = {
+  Name: '',
+  Line1: '',
+  Line2: '',
+  Line3: '',
+  DistrictOrCounty: '',
+  City: '',
+  StateOrProvinceCode: '',
+  CountryCode: '',
+  PostalCode: '',
+  PhoneNumber: '',
+}
+
+const mockCreateFulfillmentOrderItem = {
+  SellerSKU: '',
+  SellerFulfillmentOrderItemId: '',
+  Quantity: 1,
+}
+
 describe('fulfillmentOutboundShipment', () => {
-  describe('getFulfillmentPreview', () => {
-    const mockAddress = {
-      Name: '',
-      Line1: '',
-      Line2: '',
-      Line3: '',
-      DistrictOrCounty: '',
-      City: '',
-      StateOrProvinceCode: '',
-      CountryCode: '',
-      PostalCode: '',
-      PhoneNumber: '',
+  describe('createFulfillmentOrder', () => {
+    const parameters = {
+      SellerFulfillmentOrderItemId: '',
+      DisplayableOrderId: '',
+      DisplayableOrderDateTime: new Date(),
+      DisplayableOrderComment: '',
+      ShippingSpeedCategory: 'Priority',
+      DestinationAddress: mockAddress,
+      Items: [mockCreateFulfillmentOrderItem],
     }
 
+    it('returns the standard response elements if succesful', async () => {
+      expect.assertions(1)
+
+      const mockCreateFulfillmentOrder = createMockHttpClient(
+        'fulfillment_outbound_shipment_create_fulfillment_order',
+      )
+
+      expect(
+        await mockCreateFulfillmentOrder.fulfillmentOutboundShipment.createFulfillmentOrder(
+          parameters,
+        ),
+      ).toMatchSnapshot()
+    })
+
+    it('throws a parsing error when the status response isnt valid', async () => {
+      expect.assertions(1)
+
+      await expect(() =>
+        mockMwsFail.fulfillmentOutboundShipment.createFulfillmentOrder(parameters),
+      ).rejects.toStrictEqual(new ParsingError(parsingError))
+    })
+  })
+
+  describe('getFulfillmentPreview', () => {
     const mockGetFulfillmentPreviewItem = {
       SellerSKU: '',
       SellerFulfillmentOrderItemId: '',
