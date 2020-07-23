735758cac0c8c1c01bde7b91ade96750c4bde21e
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-07-23 19:34:54 +0800

    refactor: set fast-xml-parser to include attributes

--- src/http.ts
+++ src/http.ts
@@ -310,7 +310,13 @@ const parseResponse = <T>(
   response: RequestResponse,
   parseString = false,
 ): [T | string, RequestMeta] => {
-  const responseData = parseString ? response.data : parser.parse(response.data)
+  const responseData = parseString
+    ? response.data
+    : parser.parse(response.data, {
+        attributeNamePrefix: '',
+        ignoreAttributes: false,
+        attrNodeName: 'attr',
+      })
   return [
     responseData,
     {
