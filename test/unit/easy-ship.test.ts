4b85e34e184e781b9a8740db8480bd4a68cc0110
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-07-24 22:08:11 +0800

    test: unit test for createScheduledPackage

--- test/unit/easy-ship.test.ts
+++ test/unit/easy-ship.test.ts
@@ -1,18 +1,45 @@
 import { createMockHttpClient, mockMwsServiceStatus } from '../utils'
 
+const mockDimensions = {
+  Length: 1,
+  Width: 1,
+  Height: 1,
+  Unit: 'cm',
+}
+
+const mockWeight = {
+  Value: 1,
+  Unit: 'g',
+}
+
 describe('easyShip', () => {
-  describe('listPickupSlots', () => {
-    const mockDimensions = {
-      Length: 1,
-      Width: 1,
-      Height: 1,
-      Unit: 'cm',
+  describe('createScheduledPackage', () => {
+    const mockPickupSlot = {
+      SlotId: '',
+      PickupTimeStart: new Date(),
+      PickupTimeEnd: new Date(),
+    }
+    const mockPackageRequestDetails = {
+      PackagePickupSlot: mockPickupSlot,
+    }
+    const parameters = {
+      AmazonOrderId: '',
+      MarketplaceId: '',
+      PackageRequestDetails: mockPackageRequestDetails,
     }
 
-    const mockWeight = {
-      Value: 1,
-      Unit: 'g',
-    }
+    it('returns information about the scheduled package if succesful', async () => {
+      expect.assertions(1)
+
+      const mockCreateSchedulePackage = createMockHttpClient('easy_ship_create_scheduled_package')
+
+      expect(
+        await mockCreateSchedulePackage.easyShip.createScheduledPackage(parameters),
+      ).toMatchSnapshot()
+    })
+  })
+
+  describe('listPickupSlots', () => {
     const parameters = {
       MarketplaceId: '',
       AmazonOrderId: '',
