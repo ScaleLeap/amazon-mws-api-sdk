52ae3dadea4347a238b36ab8fd0e1ca7bf3cfcc9
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-07-24 20:44:08 +0800

    test: add unit test for listPickupSlots

--- test/unit/easy-ship.test.ts
+++ test/unit/easy-ship.test.ts
@@ -1,6 +1,34 @@
-import { mockMwsServiceStatus } from '../utils'
+import { createMockHttpClient, mockMwsServiceStatus } from '../utils'
 
 describe('easyShip', () => {
+  describe('listPickupSlots', () => {
+    const mockDimensions = {
+      Length: 1,
+      Width: 1,
+      Height: 1,
+      Unit: 'cm',
+    }
+
+    const mockWeight = {
+      Value: 1,
+      Unit: 'g',
+    }
+    const parameters = {
+      MarketplaceId: '',
+      AmazonOrderId: '',
+      PackageDimensions: mockDimensions,
+      PackageWeight: mockWeight,
+    }
+
+    it('returns list of pickup slots if succesful', async () => {
+      expect.assertions(1)
+
+      const mockListPickupSlots = createMockHttpClient('easy_ship_list_pickup_slots')
+
+      expect(await mockListPickupSlots.easyShip.listPickupSlots(parameters)).toMatchSnapshot()
+    })
+  })
+
   describe('getServiceStatus', () => {
     it('returns a parsed model when the status response is valid', async () => {
       expect.assertions(1)
