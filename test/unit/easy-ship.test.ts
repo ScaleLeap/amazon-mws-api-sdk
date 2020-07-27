cd074bfd49af8874ed74b2d3d43f50d1c1cb09f1
EJ Mercado <justinemmanuelmercado@gmail.com>
2020-07-27 19:05:29 +0800

    tests: add unit test using mock from c sharp lib

--- test/unit/easy-ship.test.ts
+++ test/unit/easy-ship.test.ts
@@ -1,5 +1,38 @@
 import { createMockHttpClient, mockMwsServiceStatus } from '../utils'
 
+function mockEnum() {
+  /**
+   * Mock everything in purify-ts, restore it back to normal,
+   *  except for `enumeration` which will be stubbed to accept
+   * https://github.com/facebook/jest/issues/936#issuecomment-265074320
+   */
+  const original = jest.requireActual('purify-ts')
+
+  return {
+    ...original, // Pass down all the exported objects
+    enumeration: <T extends Record<string, string | number>>(enumeration: T) => {
+      const enumValues = Object.values(enumeration)
+
+      return original.Codec.custom({
+        decode: (input: string | number) => {
+          if (typeof input !== 'string' && typeof input !== 'number') {
+            return original.Left(`Expected enum, received ${input}`)
+          }
+
+          const enumIndex = enumValues.indexOf(input)
+
+          return enumIndex !== -1 || input === 'String'
+            ? original.Right((enumValues[enumIndex] as T[keyof T]) || 'String')
+            : original.Left(`Expected enum, received ${input}`)
+        },
+        encode: original.identity,
+        schema: () => ({ enum: enumValues }),
+      })
+    },
+  }
+}
+jest.mock('purify-ts', () => mockEnum())
+
 const mockDimensions = {
   Length: 1,
   Width: 1,
@@ -27,6 +60,18 @@ describe('easyShip', () => {
       MarketplaceId: '',
       PackageRequestDetails: mockPackageRequestDetails,
     }
+
+    it('returns information about the scheduled package if structure is correct', async () => {
+      expect.assertions(1)
+
+      const mockCreateSchedulePackage = createMockHttpClient(
+        'easy_ship_create_scheduled_package_from_c_sharp',
+      )
+
+      expect(
+        await mockCreateSchedulePackage.easyShip.createScheduledPackage(parameters),
+      ).toMatchSnapshot()
+    })
 
     it('returns information about the scheduled package if succesful', async () => {
       expect.assertions(1)
