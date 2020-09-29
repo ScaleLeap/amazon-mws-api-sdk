c48d385a5eb9ea5aed098353dfee43d190271987
Roman Filippov <rf@romanfilippov.com>
2020-09-29 21:03:10 +0700

    fix: renames in README too

--- utils/rename-package.ts
+++ utils/rename-package.ts
@@ -1,12 +1,17 @@
 import { readFileSync, writeFileSync } from 'fs'
 import { join } from 'path'
 
+import { name } from '../package.json'
+
+const NEW_NAME = 'mws'
+
 function changeName(file: string) {
   const path = join(__dirname, '../', file)
-  const content = JSON.parse(readFileSync(path, { encoding: 'utf8' }))
-  Object.assign(content, { name: 'mws' })
-  writeFileSync(path, JSON.stringify(content, undefined, 2), { encoding: 'utf8' })
+  let content = readFileSync(path, { encoding: 'utf8' })
+  content = content.split(name).join(NEW_NAME)
+  writeFileSync(path, content, { encoding: 'utf8' })
 }
 
 changeName('package.json')
 changeName('package-lock.json')
+changeName('README.md')
