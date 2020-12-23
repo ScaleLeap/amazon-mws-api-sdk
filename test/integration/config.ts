04bc7eaaeace94f257868c545f1e2475bc1638ad
Roman Filippov <rf@romanfilippov.com>
2020-12-23 17:31:43 +0700

    test: adds option for marketplace for test http client

--- test/integration/config.ts
+++ test/integration/config.ts
@@ -1,9 +1,13 @@
-import { amazonMarketplaces } from '@scaleleap/amazon-marketplaces'
+import { amazonMarketplacesList } from '@scaleleap/amazon-marketplaces'
 import { BaseConfig } from '@scaleleap/config'
 
 import { HttpClient } from '../../src'
 
 export class Config extends BaseConfig {
+  private readonly MARKETPLACE_COUNTRY_CODE = this.get('MARKETPLACE_COUNTRY_CODE')
+    .default('CA')
+    .asString()
+
   private readonly AWS_ACCESS_KEY_ID = this.get('AWS_ACCESS_KEY_ID').required().asString()
 
   private readonly MWS_AUTH_TOKEN = this.get('MWS_AUTH_TOKEN').required().asString()
@@ -12,9 +16,19 @@ export class Config extends BaseConfig {
 
   private readonly SELLER_ID = this.get('SELLER_ID').required().asString()
 
-  public createHttpClient() {
+  public createHttpClient(): HttpClient {
+    const marketplace = amazonMarketplacesList.find(
+      (m) => m.countryCode.toString() === this.MARKETPLACE_COUNTRY_CODE,
+    )
+
+    if (!marketplace) {
+      throw new Error(
+        `Cannot find marketplace with country code "${this.MARKETPLACE_COUNTRY_CODE}".`,
+      )
+    }
+
     return new HttpClient({
-      marketplace: amazonMarketplaces.CA,
+      marketplace,
       awsAccessKeyId: this.AWS_ACCESS_KEY_ID,
       mwsAuthToken: this.MWS_AUTH_TOKEN,
       secretKey: this.SECRET_KEY,
