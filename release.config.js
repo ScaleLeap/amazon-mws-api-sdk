b78628cd428ecfd04092bc7f48ba1c2f42c11c29
Toan Nguyen <nguyentoandev@gmail.com>
2021-04-05 17:46:11 +0700

    ci: use successCmd hook to publish

--- release.config.js
+++ release.config.js
@@ -1,1 +1,11 @@
-module.exports = require('@scaleleap/utils/semantic-release')
+module.exports = {
+  extends: '@scaleleap/utils/semantic-release',
+  plugins: [
+    [
+      '@semantic-release/exec',
+      {
+        successCmd: 'node_modules/.bin/ts-node -T utils/rename-package.ts && npm publish',
+      },
+    ],
+  ],
+}
