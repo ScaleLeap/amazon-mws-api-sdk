7e459aa757d1c21f733c332f726bb4c4b493ce98
Toan Nguyen <nguyentoandev@gmail.com>
2021-04-06 13:28:30 +0700

    fix: release to mws package

--- release.config.js
+++ release.config.js
@@ -4,7 +4,7 @@ module.exports = {
   extends: '@scaleleap/utils/semantic-release',
   plugins: [
     ...plugins,
-    ...[
+    [
       '@semantic-release/exec',
       {
         successCmd: 'node_modules/.bin/ts-node -T utils/rename-package.ts && npm publish',
