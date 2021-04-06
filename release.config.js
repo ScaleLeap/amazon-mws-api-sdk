384eea5f02a62988e01241e3c2b3ab68c16c4036
Toan Nguyen <nguyentoandev@gmail.com>
2021-04-06 11:09:21 +0700

    chore: use semantic release exec to hook publish step

--- release.config.js
+++ release.config.js
@@ -1,1 +1,14 @@
-module.exports = require('@scaleleap/utils/semantic-release')
+const { plugins } = require('@scaleleap/semantic-release-config')
+
+module.exports = {
+  extends: '@scaleleap/utils/semantic-release',
+  plugins: [
+    ...plugins,
+    ...[
+      '@semantic-release/exec',
+      {
+        successCmd: 'node_modules/.bin/ts-node -T utils/rename-package.ts && npm publish',
+      },
+    ],
+  ],
+}
