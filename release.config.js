84fe3ed5bda77e79d74e0a74a1f81e9b2e7524e5
Toan Nguyen <nguyentoandev@gmail.com>
2021-04-06 10:17:45 +0700

    ci: revert escape new line before verify release logs (#351)
    * Revert "fix: markdown lint in README.md"
    
    This reverts commit 94501d1bfc655939271405b5d4b92d5095e6fc4e.
    
    * Revert "ci: use successCmd hook to publish"
    
    This reverts commit b78628cd428ecfd04092bc7f48ba1c2f42c11c29.
    
    * Revert "ci: document about escape multiline strings in github actions"
    
    This reverts commit f31d1bbf9caec9e1ce9dd46c70f25c2cdbd0941d.
    
    * Revert "ci: escape new line before verify release logs"
    
    This reverts commit fe5b2fd75c1b693b6e4c22c4068c895fbd7b8eaa.

--- release.config.js
+++ release.config.js
@@ -1,11 +1,1 @@
-module.exports = {
-  extends: '@scaleleap/utils/semantic-release',
-  plugins: [
-    [
-      '@semantic-release/exec',
-      {
-        successCmd: 'node_modules/.bin/ts-node -T utils/rename-package.ts && npm publish',
-      },
-    ],
-  ],
-}
+module.exports = require('@scaleleap/utils/semantic-release')
