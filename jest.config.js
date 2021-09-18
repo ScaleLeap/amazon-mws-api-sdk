06f6c81968e195fb4b891175a769cc0ac51273fa
Roman <491247+moltar@users.noreply.github.com>
2021-09-18 04:39:32 +0000

    ci: skips integration tests when env vars not set
    Closes: #252

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,24 @@
+const testPathIgnorePatterns = ['/node_modules/']
+
+// skips integration tests if the required env vars aren't set
+// this can happen:
+//
+//  - in CI, during a PR
+//  - locally during development, when .env was not set
+if (
+  !(
+    process.env.AWS_ACCESS_KEY_ID &&
+    process.env.MWS_AUTH_TOKEN &&
+    process.env.SECRET_KEY &&
+    process.env.SELLER_ID
+  )
+) {
+  testPathIgnorePatterns.push('/test/integration/')
+}
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['./test/jest.setup.js', '@scaleleap/jest-polly', './test/polly.js'],
+  testPathIgnorePatterns,
 }
